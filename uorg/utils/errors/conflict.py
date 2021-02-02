25005aee5d99c4a1d81bdd39147ef0dc72329d25
Jules Lasne <jules.lasne@gmail.com>
2021-02-02 18:42:12 +0100

    Fixed errors and success

--- uorg/utils/errors/conflict.py
+++ uorg/utils/errors/conflict.py
@@ -1,24 +1,6 @@
-"""
-    PyMatcha - A Python Dating Website
-    Copyright (C) 2018-2019 jlasne/gmorer
-    <jlasne@student.42.fr> - <lauris.skraucis@gmail.com>
-
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <https://www.gnu.org/licenses/>.
-"""
-from PyMatcha import application
-from PyMatcha.utils.errors.base_class import CustomException
-from PyMatcha.utils.errors.template import generate_error_json
+from uorg import application
+from uorg.utils.errors.base_class import CustomException
+from uorg.utils.errors.template import generate_error_json
 
 
 class ConflictError(CustomException):
